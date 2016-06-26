cf8426e5ebbce73f1cf03b1b80b5253daaf9d078
Mark Edmondson <mark@guestfolio.com>
2016-06-26 15:38:53 -0700

    iterate csv into a question hash and strand hash

--- lib/question_picker.rb
+++ lib/question_picker.rb
@@ -1,19 +1,59 @@
 require 'csv'
 
 class QuestionPicker
-  attr_accessor :questions
+  attr_accessor :strands, :csv_questions, :questions
 
-  def initialize(file_path)
-    load_questions(file_path)
+  def initialize(file_path=nil)
+    if file_path
+      load_questions(file_path)
+      load_strands
+      sort_questions
+    end
   end
 
   # Load questions from CSV file
   # @return [CSV::Table]
   #
   def load_questions(file_path)
-    @questions = CSV.read(file_path, headers: true, header_converters: :symbol, converters: :all)
+    @csv_questions = CSV.read(file_path, headers: true, header_converters: :symbol, converters: :all)
   end
 
+  # TODO
+  #
+  def sort_questions
+    # @questions = questions.sort! { |a, b| a[:difficulty] <=> b[:difficulty] }
+  end
+
+  # Transform to strand => [standards] hash eg.
+  # { 1 => [1, 2, 3], 2 => [4, 5] }
+  # @return [Hash]
+  #
+  def load_strands
+    @strands = csv_questions.inject(Hash.new { |h, k| h[k] = Array.new }) do |memo, question|
+      memo[question[:strand_id]] << question[:standard_id]
+      memo[question[:strand_id]].uniq!
+      memo
+    end
+    @strands
+  end
+
+  # Convert the questions into a hash
+  # {
+  #   <strand-id> =>
+  #     <standard-id> => [<question-ids>]
+  # }
+  # @return [Hash]
+  #
+  def questions
+    @questions ||= csv_questions.reduce(Hash.new { |h, k| h[k] = Hash.new { |h2, k2| h2[k2] = Array.new } }) do |memo, question|
+      strand = question[:strand_id]
+      standard = question[:standard_id]
+
+      memo[strand][standard] << question[:question_id]
+      memo
+    end
+    @questions
+  end
 
   # Pick questions
   # @return [Array<Integer>]
--- spec/lib/question_picker_spec.rb
+++ spec/lib/question_picker_spec.rb
@@ -4,11 +4,80 @@ require_relative '../../lib/question_picker'
 
 describe QuestionPicker do
   let(:picker)    { described_class.new }
+  let(:strands)   {
+    {
+      1 => [1, 2],
+      2 => [3]
+    }
+  }
+  let(:questions) {
+    {
+      1 => {
+        1 => [1, 2],
+        2 => [3]
+      },
+      2 => {
+        3 => [4]
+      }
+    }
+  }
+
+  before do
+    picker.questions = questions
+    picker.strands   = strands
+  end
+
   describe '#load_questions' do
     let(:file_path) { File.expand_path 'spec/fixtures/questions.csv' }
 
     it 'should return a CSV::Table' do
       expect(picker.load_questions(file_path)).to be_a CSV::Table
+    end
+  end
+
+  describe '#sort_questions' do
+    xit 'should order the questions by difficulty' do
+
+    end
+  end
+
+  describe '#load_strands' do
+    let(:csv_questions) {
+      [
+        {
+          strand_id: 1,
+          standard_id: 1,
+          question_id: 1
+        },
+        {
+          strand_id: 1,
+          standard_id: 2,
+          question_id: 2
+        }
+      ]
+    }
+
+    before { picker.csv_questions = csv_questions }
+
+    it 'should return a hash of { strand => [standards] }' do
+      expect(picker.load_strands).to eq({ 1 => [1, 2] })
+    end
+  end
+
+  describe '#questions' do
+    it 'should return a hash of { strand => {standard: [questions]} }' do
+      expect(picker.questions).to eq(
+        { 1 =>
+          {
+            1 => [1, 2],
+            2 => [3]
+          },
+          2 =>
+          {
+            3  => [4]
+          }
+        }
+      )
     end
   end
 
